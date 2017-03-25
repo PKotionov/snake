1cab6755ec218461fcdce3e08ae795091de1a344
Pasha <p-a-shok@mail.ru>
2017-03-25 17:05:34 +0300

    создали кончтруктор point (create constructor point)

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Drow();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Drow();
+
+          
+            Console.ReadLine();
+
+        }
+       
+    }
+}
