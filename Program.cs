97637fc3ee2d796499f030cc66ef3ab38f57a612
Pasha <p-a-shok@mail.ru>
2017-03-26 16:29:43 +0300

    создали класс HorizontLine, в нем создали линию из 3х точек

--- Program.cs
+++ Program.cs
@@ -11,52 +11,14 @@ namespace snakes
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-   //         p1.Drow();
+            p1.Drow();
 
             Point p2 = new Point(4, 5, '#');
-   //         p2.Drow();
+                     p2.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontLine line = new HorizontLine();
+            line.Drow();
 
-            int x = numList[ 0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            int xzopa = 10;
-            int yzopa = 10;
-
-            Point p3 = new Point(xzopa, yzopa, 'Ж');
-            Point p4 = new Point(xzopa+1, yzopa, 'O');
-            Point p5 = new Point(xzopa + 2, yzopa, 'П');
-            Point p6 = new Point(xzopa + 3, yzopa, 'A');
-
-
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            pList.Add(p6);
-
-
-
-            foreach (Point px in pList)
-            {
-           px.Drow();
-            }
-           
                
 
             
