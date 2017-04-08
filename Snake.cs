725768b017a606eef28f4b65f54c25ae583af4a2
Pasha <p-a-shok@mail.ru>
2017-04-08 15:08:48 +0300

    исправили ошибку: добавили Public к методу Snake

--- Snake.cs
+++ Snake.cs
@@ -8,7 +8,7 @@ namespace snakes
 {
     class Snake : Figure
     {
-        Snake(Point tail, int lenght, Direction direction)
+       public Snake(Point tail, int lenght, Direction direction)
         {
             pList = new List<Point>();
             for(int i = 0; i < lenght; i++)
